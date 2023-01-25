fae736b51fb96cba5e9545568534bc259b8f3d22
RiceMarch <ricemarch@foxmail.com>
2023-01-25 23:42:43 +0800

    feat: 单例模式优化

--- src/main/java/com/ricemarch/patternspring/pay/strategyFactory/StrategyFactory.java
+++ src/main/java/com/ricemarch/patternspring/pay/strategyFactory/StrategyFactory.java
@@ -4,16 +4,27 @@ package com.ricemarch.patternspring.pay.strategyFactory;
 import com.ricemarch.patternspring.pay.strategy.PayStrategy;
 import com.ricemarch.patternspring.pay.strategyEnum.StrategyEnum;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 // 我们的工厂类 依靠策略枚举返回策略类
 // 我们以后永远不需要修改我们的factory，他是一个无状态的
 public class StrategyFactory {
+
+    private static final Map<String, PayStrategy> STRATEGY_MAP = new ConcurrentHashMap<>();
+
     public static PayStrategy getPayStrategy(StrategyEnum strategyEnum) {
-        PayStrategy payStrategy = null;
-        try {
-            payStrategy = (PayStrategy) Class.forName(strategyEnum.getValue()).newInstance();
-        } catch (Exception e) {
-            // 异常
+        PayStrategy payStrategy = STRATEGY_MAP.get(strategyEnum.getValue());
+        if (payStrategy == null) {
+            try {
+
+                // 每次获取都需要进行一次反射。优化调整==> 单例模式
+                payStrategy = (PayStrategy) Class.forName(strategyEnum.getValue()).newInstance();
+                STRATEGY_MAP.put(strategyEnum.getValue(), payStrategy);
+            } catch (Exception e) {
+                // 异常
+            }
         }
         return payStrategy;
     }
